06f48ed97ea006ce601594b9d80f86a589c394c5
Kai9026 <arkaitz.lazaro.uva@gmail.com>
2022-08-13 10:40:49 +0200

    fix: add beans to configuration

--- my-bank-infrastructure/src/main/java/com/github/kai9026/mysimplebank/infrastructure/web/controller/customer/model/CustomerAddressDTO.java
+++ my-bank-infrastructure/src/main/java/com/github/kai9026/mysimplebank/infrastructure/web/controller/customer/model/CustomerAddressDTO.java
@@ -1,10 +1,13 @@
 package com.github.kai9026.mysimplebank.infrastructure.web.controller.customer.model;
 
 import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class CustomerAddressDTO {
 
